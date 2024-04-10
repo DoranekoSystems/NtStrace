1c4cd56ce0749e5525990cf4a074133b1a8ba099
Kenjiro.Ichise <ichise@doranekosystems.com>
2024-04-10 23:58:59 +0900

    Code Refactoring

--- tracer.js
+++ tracer.js
@@ -1,53 +1,50 @@
 // original software:https://github.com/sh1ma/iostrace
 // Usage: frida -l tracer.js app.exe
 
-var Color = {
-  RESET: "\x1b[39;49;00m",
-  Black: "0;01",
-  Blue: "4;01",
-  Cyan: "6;01",
-  Gray: "7;11",
-  Green: "2;01",
-  Purple: "5;01",
-  Red: "1;01",
-  Yellow: "3;01",
-  Light: {
-    Black: "0;11",
-    Blue: "4;11",
-    Cyan: "6;11",
-    Gray: "7;01",
-    Green: "2;11",
-    Purple: "5;11",
-    Red: "1;11",
-    Yellow: "3;11",
+var color = {
+  reset: '\x1b[39;49;00m',
+  black: '0;01',
+  blue: '4;01',
+  cyan: '6;01',
+  gray: '7;11',
+  green: '2;01',
+  purple: '5;01',
+  red: '1;01',
+  yellow: '3;01',
+  light: {
+    black: '0;11',
+    blue: '4;11',
+    cyan: '6;11',
+    gray: '7;01',
+    green: '2;11',
+    purple: '5;11',
+    red: '1;11',
+    yellow: '3;11',
   },
 };
 
-var LOG = function (input, kwargs) {
+var log = function (input, kwargs) {
   kwargs = kwargs || {};
-  var logLevel = kwargs["l"] || "log",
-    colorPrefix = "\x1b[3",
-    colorSuffix = "m";
-  if (typeof input === "object")
-    input = JSON.stringify(input, null, kwargs["i"] ? 2 : null);
-  if (kwargs["c"])
-    input = colorPrefix + kwargs["c"] + colorSuffix + input + Color.RESET;
+  var logLevel = kwargs['l'] || 'log',
+    colorPrefix = '\x1b[3',
+    colorSuffix = 'm';
+  if (typeof input === 'object') input = JSON.stringify(input, null, kwargs['i'] ? 2 : null);
+  if (kwargs['c']) input = colorPrefix + kwargs['c'] + colorSuffix + input + color.reset;
   console[logLevel](input);
 };
 
-var module = Process.getModuleByName("ntdll.dll");
+var module = Process.getModuleByName('ntdll.dll');
 var symbols = module.enumerateExports();
-
 var sysDict = {};
 var sysAddressList = [];
-var ThreadStepsDict = {};
-var ThreadsFollowed = {};
+var threadStepsDict = {};
+var threadsFollowed = {};
 
 for (var i = 0; i < symbols.length; i++) {
   const sysName = symbols[i].name;
   if (
-    (sysName.indexOf("_NT") == 0 || sysName.indexOf("Nt") == 0) &&
-    sysName.indexOf("Ntdll") == -1
+    (sysName.indexOf('_NT') == 0 || sysName.indexOf('Nt') == 0) &&
+    sysName.indexOf('Ntdll') == -1
   ) {
     const symAddr = symbols[i].address;
     const sysNumber = symAddr.add(0x04).readUInt();
@@ -57,7 +54,7 @@ for (var i = 0; i < symbols.length; i++) {
 }
 
 function isThreadFollowed(tid) {
-  if (ThreadsFollowed[tid]) {
+  if (threadsFollowed[tid]) {
     return true;
   } else {
     return false;
@@ -66,63 +63,62 @@ function isThreadFollowed(tid) {
 
 function onMatch(context) {
   var threadId = Process.getCurrentThreadId();
-  var ThreadSteps = ThreadStepsDict[threadId];
+  var threadSteps = threadStepsDict[threadId];
+
   // ignore frida's thread
-  if (parseInt(context.rip) + 2 != ThreadSteps[0]) {
+  if (parseInt(context.rip) + 2 != threadSteps[0]) {
     return;
   }
 
   var calledNumber = context.rax.toInt32();
   var sysName = sysDict[calledNumber];
-  LOG(
+  log(
     `${DebugSymbol.fromAddress(context.rip).moduleName}!${
       DebugSymbol.fromAddress(context.rip).address
     }`
   );
-
   var inNtDllFlag = false;
-  if (sysAddressList.indexOf(ThreadSteps[5]) != -1) {
+  if (sysAddressList.indexOf(threadSteps[5]) != -1) {
     inNtDllFlag = true;
   }
-
   if (inNtDllFlag) {
-    LOG(`[${calledNumber}]${sysName}`, { c: Color.Green });
+    log(`[${calledNumber}]${sysName}`, { c: color.green });
   } else {
-    LOG(`[${calledNumber}]${sysName}`, { c: Color.Blue });
+    log(`[${calledNumber}]${sysName}`, { c: color.blue });
   }
-  if (sysName == "NtCreateThread" || sysName == "NtCreateThreadEx") {
+  if (sysName == 'NtCreateThread' || sysName == 'NtCreateThreadEx') {
     var funcPtr = ptr(context.rsp.add(0x28).readU64());
     Interceptor.attach(funcPtr, {
       onEnter(args) {
         if (isThreadFollowed(this.threadId)) {
           return;
         }
-        FollowThread(this.threadId);
+        followThread(this.threadId);
       },
       onLeave(retVal) {
-        UnfollowThread(this.threadId);
+        unfollowThread(this.threadId);
       },
     });
   }
 }
 
-function FollowThread(tid) {
+function followThread(tid) {
   if (isThreadFollowed(tid)) {
     return;
   }
-  ThreadsFollowed[tid] = true;
-  LOG("[+] Following thread " + tid, { c: Color.Red });
+  threadsFollowed[tid] = true;
+  log('[+] Following thread ' + tid, { c: color.red });
   Stalker.follow(tid, {
     transform: function (iterator) {
       const instruction = iterator.next();
       do {
         var threadId = Process.getCurrentThreadId();
-        if (ThreadStepsDict[threadId] == undefined) {
-          ThreadStepsDict[threadId] = [0, 0, 0, 0, 0, 0];
+        if (threadStepsDict[threadId] == undefined) {
+          threadStepsDict[threadId] = [0, 0, 0, 0, 0, 0];
         }
-        ThreadStepsDict[threadId].unshift(parseInt(instruction.address));
-        ThreadStepsDict[threadId].pop();
-        if (instruction.mnemonic === "syscall") {
+        threadStepsDict[threadId].unshift(parseInt(instruction.address));
+        threadStepsDict[threadId].pop();
+        if (instruction.mnemonic === 'syscall') {
           iterator.putCallout(onMatch);
         }
         iterator.keep();
@@ -131,17 +127,17 @@ function FollowThread(tid) {
   });
 }
 
-function UnfollowThread(threadId) {
+function unfollowThread(threadId) {
   if (!isThreadFollowed(threadId)) {
     return;
   }
-  delete ThreadsFollowed[threadId];
-  LOG("[+] Unfollowing thread " + threadId, { c: Color.Red });
+  delete threadsFollowed[threadId];
+  log('[+] Unfollowing thread ' + threadId, { c: color.red });
   Stalker.unfollow(threadId);
   Stalker.garbageCollect();
 }
 
 const ths = Process.enumerateThreads();
 ths.forEach((el) => {
-  FollowThread(el.id);
+  followThread(el.id);
 });
